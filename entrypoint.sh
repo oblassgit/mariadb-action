2979e2f2df19b958d3c3db7ed8011d146184095a
19年梦醒 <3949379+getong@users.noreply.github.com>
2024-06-23 15:49:06 +0800

    Revert "Add Mariadb image option (#8)"
    This reverts commit e41022399005dcf599656e5ecef61e6276f85b08.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
-declare -a docker_run=( docker run )
+docker_run="docker run"
 
 if [ -n "$INPUT_MYSQL_ROOT_PASSWORD" ]; then
   echo "Root password not empty, use root superuser"
 
-  docker_run+=( -e MYSQL_ROOT_PASSWORD="$INPUT_MYSQL_ROOT_PASSWORD" )
+  docker_run="$docker_run -e MYSQL_ROOT_PASSWORD=$INPUT_MYSQL_ROOT_PASSWORD"
 elif [ -n "$INPUT_MYSQL_USER" ]; then
   if [ -z "$INPUT_MYSQL_PASSWORD" ]; then
     echo "The mysql password must not be empty when mysql user exists"
@@ -14,36 +14,29 @@ elif [ -n "$INPUT_MYSQL_USER" ]; then
 
   echo "Use specified user and password"
 
-  docker_run+=( -e MYSQL_RANDOM_ROOT_PASSWORD=true -e MYSQL_USER="$INPUT_MYSQL_USER" -e MYSQL_PASSWORD="$INPUT_MYSQL_PASSWORD" )
+  docker_run="$docker_run -e MYSQL_RANDOM_ROOT_PASSWORD=true -e MYSQL_USER=$INPUT_MYSQL_USER -e MYSQL_PASSWORD=$INPUT_MYSQL_PASSWORD"
 else
   echo "Using empty password for root"
 
-  docker_run+=(-e MYSQL_ALLOW_EMPTY_PASSWORD=true )
+  docker_run="$docker_run -e MYSQL_ALLOW_EMPTY_PASSWORD=true"
 fi
 
 if [ -n "$INPUT_MYSQL_DATABASE" ]; then
   echo "Use specified database"
 
-  docker_run+=( -e MYSQL_DATABASE="$INPUT_MYSQL_DATABASE" )
+  docker_run="$docker_run -e MYSQL_DATABASE=$INPUT_MYSQL_DATABASE"
 fi
 
-HEALTHCHECK_INTERVAL=3
-docker_run+=( --health-cmd='healthcheck.sh --connect --innodb_initialized' --health-start-period=5s --health-start-interval="$HEALTHCHECK_INTERVAL"s )
-docker_run+=( -d -p "$INPUT_HOST_PORT:$INPUT_CONTAINER_PORT" "$INPUT_MARIADB_IMAGE:$INPUT_MARIADB_VERSION" --port="$INPUT_CONTAINER_PORT" )
-docker_run+=( --character-set-server="$INPUT_CHARACTER_SET_SERVER" --collation-server="$INPUT_COLLATION_SERVER" )
+docker_run="$docker_run --health-cmd='healthcheck.sh --connect --innodb_initialized'"
+docker_run="$docker_run -d -p $INPUT_HOST_PORT:$INPUT_CONTAINER_PORT mariadb:$INPUT_MARIADB_VERSION --port=$INPUT_CONTAINER_PORT"
+docker_run="$docker_run --character-set-server=$INPUT_CHARACTER_SET_SERVER --collation-server=$INPUT_COLLATION_SERVER"
 
-CONTAINER_NAME=$( "${docker_run[@]}" )
-
-if [ -z "$CONTAINER_NAME" ]; then
-	echo "No container started"
-	exit 1
-fi
+CONTAINER_NAME=$(eval "$docker_run" )
 
 echo "Waiting for container $CONTAINER_NAME to start..."
 
 # Loop until the container is healthy
-count=30
-until (( count == 0 )); do
+while true; do
     # Get the health status of the container
     HEALTH=$(docker inspect --format='{{.State.Health.Status}}' "$CONTAINER_NAME" 2>/dev/null)
 
@@ -56,13 +49,8 @@ until (( count == 0 )); do
         break
     else
         echo "Container $CONTAINER_NAME is still starting (current status: $HEALTH)"
-        sleep "$HEALTHCHECK_INTERVAL"
+        sleep 1
     fi
-    count=$(( count - 1 ))
 done
 
-if [ "$HEALTH" = "healthy" ]; then
-    echo "Container $CONTAINER_NAME is now started and healthy."
-else
-    echo "Container fails to start in time"
-fi
+echo "Container $CONTAINER_NAME is now started and healthy."
